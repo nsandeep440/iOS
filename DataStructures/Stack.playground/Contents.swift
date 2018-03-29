e1156f9dcaa6bf7a7aedb5f1a051f8a674ee2dab
Sandeep Nakka <nsandeep440@gmail.com>
2018-03-30 02:38:26 +0530

    Stack LIFO added

--- DataStructures/Stack.playground/Contents.swift
+++ DataStructures/Stack.playground/Contents.swift
@@ -4,7 +4,7 @@ import UIKit
 
 var str = "Hello, playground"
 
-public struct NSStack<T> {
+public struct SNStack<T> {
     // Create Array and use it for stacking
     // LIFO
     fileprivate var array = Array<T>()
@@ -34,9 +34,46 @@ public struct NSStack<T> {
      Removes and returns the last element
      */
     public mutating func pop() -> T? {
-        return array.last
+        return array.popLast()
     }
     
-    
-    
+    /**
+     Returns the top element on the stack
+     */
+    public func topElement() -> T? {
+        return array.last
+    }
 }
+
+// Int type
+var stackedArray = SNStack<Int>()
+print(stackedArray.isEmpty)
+stackedArray.push(4)
+stackedArray.push(10)
+stackedArray.push(20)
+print(stackedArray)
+print(stackedArray.topElement())
+stackedArray.pop()
+print(stackedArray)
+print(stackedArray.count)
+
+// Any type
+var strStack = SNStack<Any>()
+strStack.push("First")
+strStack.push(5)
+strStack.push(4.5)
+strStack.push(stackedArray)
+print(strStack)
+print(strStack.topElement())
+strStack.pop()
+print(strStack)
+
+
+
+
+
+
+
+
+
+
